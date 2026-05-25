3299409f9a90fc434439ccad87ecf79531b878f1
Dwayne Heraclio <dwaynehc7@icloud.com>
2026-05-24 20:42:28 -0400

    Initial portfolio upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const galleryImages = document.querySelectorAll('.gallery-grid img');
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+galleryImages.forEach(image => {
+  image.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = image.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.style.display = 'none';
+  }
+});
